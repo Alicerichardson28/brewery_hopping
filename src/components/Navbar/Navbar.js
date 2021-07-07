e69deab7e8afd19c68b14915729747a5ac211f55
Parada <paradafaii28@gmail.com>
2021-07-07 13:34:32 -0400

    style logo always be center in css

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,33 @@
 import React from 'react'
-import { Nav, NavLink, Logo } from "./NavbarElements"
+import {
+  Nav,
+  NavLink,
+  Logo,
+  NavContainer,
+  LogoContainer,
+} from "./NavbarElements";
 import logo from '../../Asset/images/logo.png'
 
 const Navbar = () => {
     return (
-        <Nav>
-            <Logo src={logo} alt="brewery-logo"/>
+      <>
+        <NavContainer>
+          <Nav>
+            <NavLink to="/">icon</NavLink>
+            <NavLink to="/">icon</NavLink>
+          </Nav>
+          <LogoContainer>
+            <Logo src={logo} alt="brewery-logo" />
+          </LogoContainer>
+          <Nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="/">About</NavLink>
             <NavLink to="/">Beer</NavLink>
             <NavLink to="/">Breweries</NavLink>
-        </Nav>
-    )
+          </Nav>
+        </NavContainer>
+      </>
+    );
 }
 
 
--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -2,26 +2,36 @@ import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 
 
+export const NavContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #eeae08;
+    height: 60px;
+`;
+
 export const Nav = styled.div`
-  background-color: #eeae08;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  height: 60px;
 `;
+
+
+
 export const NavLink = styled(Link)`
-  @import url("https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap");
-  font-family: "Karla", sans-serif;
-  color: #ffffff;
-  text-decoration: none;
-  padding: 20px;
-  font-size: 16px;
+    @import url("https://fonts.googleapis.com/css2?family=Karla:wght@300&display=swap");
+    font-family: "Karla", sans-serif;
+    color: #ffffff;
+    text-decoration: none;
+    padding: 10px;
+    font-size: 16px;
 `;
+
+export const LogoContainer = styled.div`
+    margin-left: 85px;
+`
+
 export const Logo = styled.img`
-  margin-top: 60px;
-  margin-right: 15px;
-  width: 90px;
-  border-radius: 50%;
-  border-style: solid;
-  border-color: #ffffff;
+    margin-top: 60px;
+    width: 90px;
+    border-radius: 50%;
+    border-style: solid;
+    border-color: #ffffff;
 `;
